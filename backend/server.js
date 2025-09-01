469c7dc1cc0abb45ecde241dd2e54b15b2741754
Vermin84 <verminator84@gmail.com>
2025-09-01 20:37:35 +0300

    workflow

--- backend/server.js
+++ backend/server.js
@@ -40,10 +40,10 @@ io.on("connection", (socket) => {
 // Подключение к MySQL
 // ===========================
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "mysql.railway.internal",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "testdb",
+  password: process.env.DB_PASSWORD || "DzEOZoNrzVwqtdIplsJYWkwtbZJTMGkJ",
+  database: process.env.DB_NAME || "railway",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
